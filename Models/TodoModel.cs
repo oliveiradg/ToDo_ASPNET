42048d2d83edb4292dd1bd12e0962e8438c17fd4
João Oliveira <j.oliveiradg@gmail.com>
2023-05-01 18:40:35 -0300

    [DIA 113] feat: Adicionando Suporte a Controller

--- Models/TodoModel.cs
+++ Models/TodoModel.cs
@@ -3,7 +3,7 @@
     public class TodoModel
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string? Title { get; set; }
         public bool Done { get; set; }
 
         public DateTime CreatedAt { get; set; }
