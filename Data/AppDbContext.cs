5cc2673c0c64dce250a5410557f6ff457be9e687
João Oliveira <j.oliveiradg@gmail.com>
2023-05-01 18:16:35 -0300

    [DIA 113] feat: Configurando EF e Criando banco de dados

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using TodoList.Models;
+
+namespace TodoList.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<TodoModel> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) 
+            => options.UseSqlite("DataSource=app.db;Cache=Shared");
+    }
+}
--- Models/TodoModel.cs
+++ Models/TodoModel.cs
@@ -2,5 +2,10 @@
 {
     public class TodoModel
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Done { get; set; }
+
+        public DateTime CreatedAt { get; set; }
     }
 }
